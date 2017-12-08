c9ac59b63c9c3f3a2d4bb3b84a3878b3c8279231
maxime <bisotto.maxime@gmail.com>
2017-12-08 05:23:54 +0100

    test 6

--- inscription.php
+++ inscription.php
@@ -13,7 +13,7 @@ if ($_POST['password'] == $_POST['confirmPassword']) {
     or die('Problème de sélection BD : ' . mysqli_error($link));
     $message = '<p><h1>Accident</h1><p>Un accident a eu lieu à ' . htmlspecialchars($_POST['lieu']) . ' à ' . date("H:i") . '</p></p>';
     $query = 'INSERT INTO ORGANISATEUR(\'NOM\', \'PRENOM\', \'PASSWD\', \'EMAIL\', \'TEL\') VALUES 
-  (\'' . htmlspecialchars($_POST['nom']) . '\', \'' . htmlspecialchars($_POST['prenom']) . '\', MD5(' . htmlspecialchars($_POST['password']) . 'fin\') . \', \''
+  (\'' . htmlspecialchars($_POST['nom']) . '\', \'' . htmlspecialchars($_POST['prenom']) . '\', MD5(' . htmlspecialchars($_POST['password']) . 'fin\') \', \''
         . htmlspecialchars($_POST['email']) . '\', \'' . htmlspecialchars($_POST['tel']) . '\')';
     $resultat = mysqli_query($link, $query);
     if (!$resultat) {
