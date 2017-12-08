925722408c60d49be950e9c718680f9bdeb83fb2
jy-roda <ji.igrec@gmail.com>
2017-12-08 05:58:12 +0100

    Un peu de backend

--- addevent.php
+++ addevent.php
@@ -8,7 +8,11 @@
 session_start();
 
 if (isset($_SESSION['logged']) && $_SESSION['logged'] === true) {
-    //TODO faire requetes
+    $bdd = new PDO('mysql:host=localhost;dbname=phpmyadmin;charset=utf8', 'phpmyadmin', 'nuitinfo');
+    $bdd->query("INSERT INTO EVENTS('nom','debut','fin','site') VALUES ('" . $_POST['nom'] ."', '".
+        date ("Y-m-d H:i:s", date_create_from_format("j F, YGi", $_POST['debut'] . $_POST['hdebut'])) .
+        "', '" . date ("Y-m-d H:i:s", date_create_from_format("j F, YGi", $_POST['fin'] . $_POST['hfin'])) . "', '')");
+    header("Location: /success.php",TRUE,303);
 } else {
-    //TODO Erreur login :( 
+    //TODO Erreur login :(
 }
