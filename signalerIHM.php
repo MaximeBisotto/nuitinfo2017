6a6a7fb9f20c9cb13f667f4b3c910cc2b46da998
jy-roda <ji.igrec@gmail.com>
2017-12-08 07:21:11 +0100

    Fix bdd :/

--- signalerIHM.php
+++ signalerIHM.php
@@ -6,25 +6,13 @@
 </head>
 <body>
 <?php
-echo 'a';
-$link = mysqli_connect('localhost', 'phpmyadmin', 'nuitinfo')
-    or die('Problème de connection serveur : ' . mysqli_connect_error());
-mysqli_select_db($link, 'phpmyadmin')
-    or die('Problème de sélection BD : ' . mysqli_error($link));
-$message = '<p><h1>Accident</h1><p>Un accident a eu lieu à ' . htmlspecialchars($_POST['lieu']) . ' à ' . date("H:i"). '</p></p>';
-echo 'b';
-$query = 'SELECT * FROM ALERTE';
-$resultat = mysqli_query($link, $query);
-if (!$resultat) {
-    echo 'Impossible d\'executer la requete ', $query, ' : ', mysqli_error($link);
-}
-echo 'c';
-while($a=$resultat->fetch())
-{
-    echo $a['MESSAGE_ALERT'];
+$bdd = new PDO('mysql:host=localhost;dbname=phpmyadmin;charset=utf8', 'phpmyadmin', 'nuitinfo');
+//$message = '<p><h1>Accident</h1><p>Un accident a eu lieu à ' . htmlspecialchars($_POST['lieu']) . ' à ' . date("H:i"). '</p></p>';
+$reponse = $bdd->query('SELECT * FROM ALERTE');
+while ($donnees = $reponse->fetch()) {
+    echo $donnees['MESSAGE_ALERT']. PHP_EOL;
 }
 //$resultat->closeCursor();
-echo 'd';
 ?>
     <form action="signaler.php" method="post">
         Signaler le lieu du problème: <input type="text" name="lieu">
