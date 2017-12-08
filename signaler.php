571d613443291e7ea62cd9530367e0d7fe0fa641
maxime <bisotto.maxime@gmail.com>
2017-12-08 02:22:19 +0100

    ajout insertion alerte BD

--- signaler.php
+++ signaler.php
@@ -6,6 +6,15 @@
  * Time: 00:27
  */
 
-    ajoutBD('<p><h1>Accident</h1><p>Un accident a eu lieu à ' . htmlspecialchars($_POST['lieu']) . ' à ' . date("H:i"). '</p></p>');
+$link = mysqli_connect('localhost', 'phpmyadmin', 'nuitinfo')
+    or die('Problème de connection serveur : ' . mysqli_connect_error());
+mysqli_select_db($link, 'phpmyadmin')
+    or die('Problème de sélection BD : ' . mysqli_error($link));
+$message = '<p><h1>Accident</h1><p>Un accident a eu lieu à ' . htmlspecialchars($_POST['lieu']) . ' à ' . date("H:i"). '</p></p>';
+$query = "INSERT INTO `ALERTE`(`MESSAGE_ALERT`) VALUES (" . $message . ")";
+$resultat = mysqli_query($link, $query);
+if (!$resultat) {
+    echo 'Impossible d\'executer la requete ', $query, ' : ', mysqli_error($link);
+}
     header("index.html")
 ?>
